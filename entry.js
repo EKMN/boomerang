2b178d3215e7e43e3054db97ed3743a500636450
Ekman <johannes.ekman@cgi.com>
2018-05-31 14:44:37 +0300

    initial commit (added basic files and modules)

---
+++ entry.js
@@ -0,0 +1,4 @@
+// const ora = require('ora') // spinner
+// const signale = require('signale') // logger
+
+require('./lib/scraper')()
---
+++ tests/mocks/example.js
@@ -0,0 +1,4 @@
+// const nock = require('nock')
+
+// 1. read html (fs)
+// 2. output html into a mock
